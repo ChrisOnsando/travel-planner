9bce5b1c341753cde673a29bb0fa76dcbc643c79
ChrisOnsando <chrisnemwel@gmail.com>
2025-03-19 14:39:10 +0300

    chore(project): fix deployment issues

--- travelplanner/settings.py
+++ travelplanner/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
+        'LOCATION': os.getenv('REDIS_URL'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
